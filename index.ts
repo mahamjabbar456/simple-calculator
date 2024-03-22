fa594ad824323e194f465f08e63f491b02196d75
mahamjabbar456 <mahamjabbar320@gmail.com>
2024-03-22 13:32:00 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,30 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([{
+    message: "Enter your first number : ", type: "number", name: "firstnumber"
+},
+{ message: "Enter your second number : ", type: "number", name: "secondnumber" },
+{ message: "Select one of the operator which you want to perform in this time:", type: "list", name: "operator", choices: ["Addition", "Subtraction", "Multiplication", "Division","Exponention"] },
+])
+
+let result:number;
+if(answer.operator ==="Addition"){
+    result = answer.firstnumber + answer.secondnumber;
+    console.log(`Your answer is ${result}`);
+}else if(answer.operator ==="Subtraction"){
+    result = answer.firstnumber - answer.secondnumber;
+    console.log(`Your answer is ${result}`);
+}else if(answer.operator ==="Multiplication"){
+    result = answer.firstnumber * answer.secondnumber;
+    console.log(`Your answer is ${result}`);
+}else if(answer.operator ==="Division"){
+    result = answer.firstnumber / answer.secondnumber;
+    console.log(`Your answer is ${result}`);
+} else if(answer.operator ==="Exponention"){
+    result = answer.firstnumber ** answer.secondnumber;
+    console.log(`Your answer is ${result}`);
+}else{
+    console.log("You enter invalid operator.");
+}
